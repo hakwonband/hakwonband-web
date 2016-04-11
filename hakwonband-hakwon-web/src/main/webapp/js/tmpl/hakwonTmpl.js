4c02531d89ac4164d41bd9ae686cb0a462b747e8
bdkang <hakwonband@naver.com>
2016-04-11 15:48:29 +0900

    학생 학부모 선생님 정보 없애고 교육정보배너만들었음

--- hakwonband-hakwon-web/src/main/webapp/js/tmpl/hakwonTmpl.js
+++ hakwonband-hakwon-web/src/main/webapp/js/tmpl/hakwonTmpl.js
@@ -290,11 +290,17 @@ hakwonTmpl.dashBoard = {
 		, '</div>'
 		, '<div class="col-md-6">'
 		, '	<table class="table dashboard_counting" summary="학원의 선생님, 학생, 학부모의 수를 나타냅니다">'
-		, '		<caption>학원 인원 정보</caption>'
+		, '		<caption>학원 필요 정보</caption>'
 		, '		<tbody>'
 		, '			<tr>'
 		, '				<td>'
-		, '					<span class="count_num btn-warning">{{userCount.teacher_count}}</span> 선생님'
+		                
+    // 광고배너추가
+	    , '               <a href="#"> <img src="../images/gyengsi.png" border="0"> </a>'
+
+	/*학생학부모명수			
+	                        <span class="count_num btn-warning">{{userCount.teacher_count}}</span> 선생님'            
+
 		, '				</td>'
 		, '			</tr>'
 		, '			<tr>'
@@ -304,7 +310,8 @@ hakwonTmpl.dashBoard = {
 		, '			</tr>'
 		, '			<tr>'
 		, '				<td>'
-		, '					<span class="count_num btn-info">{{userCount.parents_count}}</span> 학부모'
+		, '					<span class="count_num btn-info">{{userCount.parents_count}}</span> 학부모'                  
+	 학생학부모명수*/		
 		, '				</td>'
 		, '			</tr>'
 		, '		</tbody>'
